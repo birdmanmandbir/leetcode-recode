cd473ad268b654300f42421fa3ec25518df42777
birdmanmandbir <1522674529@qq.com>
2020-05-08 21:41:04 +0800

    some

---
+++ leetleet/lc79单词搜索/dfs.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+// @深度优先搜索
+class Solution {
+public:
+    // https://leetcode-cn.com/problems/word-search/solution/cdfsdi-gui-hui-su-by-lorwin/
+    // tips：dfs函数用实参，不要用形参，不然效率差10倍（时间and空间） 【原理：实参传递的是指针或引用】
+    bool exist(vector<vector<char>>& board, string word) {
+        if(word=="")return false;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                // 因为所有点都可以作为起始点，所以对每个点进行dfs
+                if(dfs(board, word, i, j, 0))return true;
+            }
+        }
+        return false;
+    }
+    // k is index in word
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int k){
+        int m=board.size();
+        int n=board[0].size();
+        if(i<0||i>=m||j<0||j>=n)return false;
+        if(board[i][j]!=word[k])return false;
+        if(k==word.size()-1)return true;
+        // 保证仅递归时此处标记为'/'
+        char tmp=board[i][j];
+        board[i][j]='/';
+        bool res=dfs(board, word, i+1, j, k+1)||
+            dfs(board, word, i-1, j, k+1)||
+            dfs(board, word, i, j+1, k+1)||
+            dfs(board, word, i, j-1, k+1);
+        board[i][j]=tmp;
+        return res;
+    }
+};
--- 题型总结/lc138单词拆分/try1.cpp
+++ 题型总结/lc138单词拆分/try1.cpp
@@ -1,8 +1,39 @@
+// https://leetcode-cn.com/problems/word-break/solution/139-by-ikaruga/
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-
+    bool wordBreak(string s, vector<string>& wordDict)
+    {
+        size_t validEnd = 0; // 匹配单词的最后一个结尾
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (size_t i = 0; i < s.size(); i++) {
+            if (!dp[i]) {
+                continue;
+                // 因为后面遍历worddict是建立在i之前是valid的状态下
+            }
+            // dp[i] 表示第 i 个字符前，所有的字符是否可以按字典拆分成单词
+            for (auto& word : wordDict) {
+                size_t newEnd = i + word.size();
+                if (newEnd > s.size()) {
+                    continue; // 可能有别的更短的单词
+                }
+                if (memcmp(&s[i], &word[0], word.size()) == 0) {
+                    // FIXME 可能有其它也匹配的单词，所以不在此处结束；但validEnd作用是什么，如何避免选中了使结果不匹配的单词呢？
+                    // 样例：san a {"san","s","ana"}
+                    dp[newEnd] = true;
+                    validEnd = max(validEnd, newEnd);
+                }
+            }
+        }
+        return dp.back();
     }
 };
+
+int main()
+{
+    string s = "sana";
+    vector<string> wordDict = { "san", "s", "ana" };
+    (new Solution())->wordBreak(s, wordDict);
+}
